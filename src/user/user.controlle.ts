24b103c0c4c1c560d9fc0452c3ed1832fb505e35
rafatu07 <rafa.tu@hotmail.com>
2024-05-26 21:42:47 -0300

    API nestJS

---
+++ src/user/user.controlle.ts
@@ -0,0 +1,45 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put } from "@nestjs/common";
+
+@Controller('users')
+export class UserController {
+
+    @Post()
+    async create(@Body() body) {
+        return {body};
+    }
+
+    @Get()
+    async list() {
+        return {users:[]};
+    }
+    
+    @Get(':id')
+    async show(@Param() params) {
+        return {users:{}, params};
+    }
+
+    @Put(':id')
+    async update(@Body() body, @Param() params){
+        return {
+            method: 'put',
+            body,
+            params
+        }
+    }
+    
+    @Patch(':id')
+    async updateParcial(@Body() body, @Param() params){
+        return {
+            method: 'patch',
+            body,
+            params
+        }
+    }
+
+    @Delete(':id')
+    async delete(@Param() params) {
+        return {
+            params
+        }
+    }
+}
